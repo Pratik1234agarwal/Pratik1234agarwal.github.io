f7db6d0a1e13813f9817c39eeff6963536c66aff
Pratik Agarwal <pratik1234agarwal@gmail.com>
2019-05-03 15:28:49 +0530

    Add files via upload

---
+++ build/precache-manifest.7d533cd05dff9cfc236619bcded656bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce3c466aae627c189be9a63e2959b4d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "e5542b30d51e902c2e51",
+    "url": "/static/css/main.afa8b28d.chunk.css"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "e5542b30d51e902c2e51",
+    "url": "/static/js/main.49985fa1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
